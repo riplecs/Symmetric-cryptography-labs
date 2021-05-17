d78ab94a1d505557abee35387bdc6b189a0466e4
riplecs <48178483+riplecs@users.noreply.github.com>
2021-05-17 20:59:57 +0300

    Update laba4.py

--- laba4.py
+++ laba4.py
@@ -4,16 +4,43 @@ Created on Tue Apr  6 11:38:02 2021
 
 @author: RIPLECS
 """
-import numpy as np
-import math
+
 from sympy import symbols, solve, Eq, sqrt
+import time 
+import math
+
     
+p1=0.25
+p2=0.5
+
+t_λ=2.326347874
+
+'''
 f=open('z.txt', 'r')
 z=f.read()
-
 L1=(0, 3)
 L2=(0, 1, 2, 6)
 L3=(0, 1, 2, 5)
+n1=25
+n2=26
+n3=27
+t_β1=5.419983175
+t_β2=5.542594058
+t_β3=5.662697617
+'''
+
+
+z='01011101000101001000000010100000010001100011111111011110010001011000111000100101100111100011010001100101110001101100110010110110100010001101000101000010110101100001110101111011110000010001101111000110111100100101101100001011010011110010101111111100100100111100000000011110100000000011011011111101000010101011000110001110111011001011100100011000100110111111000101101000000111100001010011100011010110100011101111100001100011011000010011101100101000011111110010000001000111010000100000111001000101111101'
+L1=(0, 2)
+L2=(0, 1, 3, 4)
+L3=(0, 3)
+n1=11
+n2=9
+n3=10
+t_β1=3.297193346
+t_β2=2.885634912
+t_β3=3.097269078
+
 
 def lfsr(state, taps, n):
     it=1
@@ -32,36 +59,25 @@ def lfsr(state, taps, n):
             break
     yield res
 
+    
 def Geffe(x0, y0, s0):
     res=''
-    for state in lfsr(x0, L1, math.inf):
+    for state in lfsr(x0, L1, N1):
         x=state
-    for state in lfsr(y0, L2, math.inf):
+    for state in lfsr(y0, L2, N2):
         y=state
-    for state in lfsr(s0, L3, math.inf):
+    for state in lfsr(s0, L3, N3):
         s=state
     for i in range(min(len(x), len(y), len(s))):
         res=res+(x[i] if s[i]=='1' else y[i])
     return res[:len(z)]
 
 
-p1=0.25
-p2=0.5
-t_λ=2.326347874
-x, y = symbols('x, y')
-#C=N*p1+t_λ*math.sqrt(N*p1*(1-p1))
-#t_β=(N*p2-C)/math.sqrt(N*p2*(1-p2))
-
-
-t_β=5.419983175
-eq1=Eq(x*p1+t_λ*sqrt(x*p1*(1-p1))-y, 0.001)
-eq2=Eq((x*p2-y)/sqrt(x*p2*(1-p2))-t_β, 0.001)
-N1, C1 = solve([eq1, eq2], [x, y])[1]
-
-t_β=5.542594058
-eq1=Eq(x*p1+t_λ*sqrt(x*p1*(1-p1))-y, 0.001)
-eq2=Eq((x*p2-y)/sqrt(x*p2*(1-p2))-t_β, 0.001)
-N2, C2 = solve([eq1, eq2], [x, y])[1]
+def calculation(t_β):
+    x, y = symbols('x, y')
+    eq1=Eq(x*p1+t_λ*sqrt(x*p1*(1-p1))-y, 0.001)
+    eq2=Eq((x*p2-y)/sqrt(x*p2*(1-p2))-t_β, 0.001)
+    return solve([eq1, eq2], [x, y])[1]
 
 
 def R(vec, c):
@@ -73,34 +89,61 @@ def R(vec, c):
     else:
         return True
  
-def bin_array(N):
-    return (np.arange(1<<N)[:, None] >> np.arange(N)[::-1]) & 0b1
 
 def find_states(n, N, C, L):
     v=(n-1)*'0'+'1'
     for state in lfsr(v, L, math.inf):
         state0=state
-    check=R(state0, C)
     candidates=[]
     for i in range(len(state0)-int(N)):
-        check=R(state0[i:int(N)+i], C)
+        s=state0[i:int(N)+i]
+        check=R(s, C)
         if check is True:
-            candidates.append(state0[i:int(N)+i][:n])
+            candidates.append(s[:n])
     return candidates
 
-candidates1=find_states(25, N1, C1, L1)
-candidates2=find_states(26, N2, C2, L2)
+
+def Next_Vector(vect):
+    n=len(vect)
+    if vect.count('0')==0:
+        return n*'0'
+    elif vect[-1]=='0':
+        return vect[:n-1]+'1'
+    else:
+        i, k=-1, 0
+        while vect[i]!='0':
+            i-=1
+            k+=1
+        return vect[:n-k-1]+'1'+'0'*k
+    
 
 def find_l3(n, l1, l2):
-    vectors = bin_array(n)
-    for i in l1:
-        for j in l2:
-            for v in vectors[1:]:
-                v=str(v)
-                G=Geffe(i, j, v[1:len(v)-1:2])
-                check=R(G[:len(z)], 1)
+    v='0'*(n-1)+'1'
+    check=False
+    while check is False:
+        for i in l1:
+            for j in l2:
+                G=Geffe(i, j, v)
+                check=R(G, 1)
                 if check is True:
-                    return i, j, v[1:len(v)-1:2]
+                    return i, j, v
+                    break
+        v=Next_Vector(v)
                 
-print(find_l3(27, candidates1, candidates2))
+            
+start_time = time.time() 
+
+
+N1, C1 = calculation(t_β1)
+N2, C2 = calculation(t_β2)
+N3, C3 = calculation(t_β3)
+
+
+candidates1=find_states(n1, N1, C1, L1)
+candidates2=find_states(n2, N2, C2, L2)
+
+
+       
+print(find_l3(n3, candidates1, candidates2))
+print("--- %s seconds ---" % (time.time() - start_time))
 
